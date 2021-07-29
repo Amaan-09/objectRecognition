b1de120fc0eb29f2a70695ebf90c555b783c2e85
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-07-29 18:04:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,26 @@ function take_snapshot() {
 }
 
 console.log("ml5 version:" , ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uQyJSGD94/',modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/uQyJSGD94/model.json',modelLoaded);
+
+function modelLoaded() 
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_object_name").innerHTML= results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(3);
+    }
+}
